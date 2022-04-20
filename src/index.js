07cd192ddbd41e6fb077f85ff98c0313144ee47f
Fran Dieguez <fran.dieguez@situm.com>
2022-04-20 22:40:58 +0200

    Migrate to SCSS

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 
 import App from "./App";
-import "./index.css";
+import "./index.scss";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 const rootElement = document.getElementById("root");
