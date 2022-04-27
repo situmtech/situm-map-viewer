6adffb4f8e29133d67f8ebae6049dd2b445c50d1
Fran Dieguez <fran.dieguez@situm.com>
2022-04-27 23:23:12 +0200

    Add minimal styling in the error page

--- src/components/ErrorFallback.jsx
+++ src/components/ErrorFallback.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
-const ErrorFallback = ({error, resetErrorBoundary}) => {
-        return (
-          <div role="alert">
-            <p>Something went wrong:</p>
-            <pre>{error.message}</pre>
-            <button onClick={resetErrorBoundary}>Try again</button>
-          </div>
-        )
-      }
+const ErrorFallback = ({ error, resetErrorBoundary }) => {
+  return (
+    <div role="alert" className="error-page">
+      <p>There was an error in the application:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Reload</button>
+    </div>
+  );
+};
 export default ErrorFallback;
