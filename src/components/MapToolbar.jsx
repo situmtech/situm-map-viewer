2b646b9f6773dc1703dbefbf559269edde7af76b
Fran Dieguez <fran.dieguez@situm.com>
2022-04-28 01:15:10 +0200

    Add a reset to north button in Map toolbar

--- src/components/MapToolbar.jsx
+++ src/components/MapToolbar.jsx
@@ -3,9 +3,45 @@ import { AiOutlineZoomIn as PlusIcon } from "react-icons/ai";
 import { AiOutlineZoomOut as MinusIcon } from "react-icons/ai";
 import { MdOutlineCenterFocusWeak as CenterIcon } from "react-icons/md";
 
-function MapToolbar({ onIncreaseZoom, onDecreaseZoom, onCenter }) {
+function MapToolbar({
+  onIncreaseZoom,
+  onDecreaseZoom,
+  onCenter,
+  onResetBearing,
+}) {
   return (
     <div className="map-toolbar">
+      <div className="map-toolbar__button" onClick={onResetBearing}>
+        <svg
+          className="map-toolbar__button__icon"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24px"
+          height="24px"
+          viewBox="0 0 32 32"
+          class="injected-svg"
+          data-src="https://content.locuslabs.com/ds-assets/icons/compass.color.svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          style={{
+            flexShrink: 0,
+            backfaceVisibility: "hidden",
+            padding: ".3rem",
+          }}
+          stroke-width="0"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M16.3832 25.6336C16.1717 26.1257 15.8258 26.1186 15.6174 25.6336L12.0689 17.3759C11.8575 16.8838 12.15 16.4849 12.7097 16.4849H19.2909C19.8562 16.4849 20.1401 16.8909 19.9317 17.3759L16.3832 25.6336Z"
+            fill="white"
+          ></path>
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M16.3832 6.36638C16.1717 5.87432 15.8258 5.88143 15.6174 6.36638L12.0689 14.6241C11.8575 15.1162 12.15 15.5151 12.7097 15.5151H19.2909C19.8562 15.5151 20.1401 15.1091 19.9317 14.6241L16.3832 6.36638Z"
+            fill="#E93232"
+          ></path>
+        </svg>
+      </div>
       <div className="map-toolbar__button map-toolbar__button--compound">
         <div className="increase-zoom" onClick={onIncreaseZoom}>
           <PlusIcon className="map-toolbar__button__icon increase-zoom-icon" />
