d532c79eb051e9f152380a6028c64a1281874e4b
Fran Dieguez <fran.dieguez@situm.com>
2022-04-28 00:17:42 +0200

    Refactor poi selector styles and compotenize its content

--- src/components/selectors/PoiSelector.jsx
+++ src/components/selectors/PoiSelector.jsx
@@ -3,59 +3,36 @@ import { FaSearch as SearchIcon } from "react-icons/fa";
 import { FaChevronCircleLeft as BackIcon } from "react-icons/fa";
 import { FaMapPin as PositionIcon } from "react-icons/fa";
 
-const PoiSelector = ({
-  buildings,
-  currentBuilding,
-  onSelect,
-  currentPoiID,
-}) => {
-  const [filterText, setFilterText] = useState("");
-  const [pois, setPois] = useState([]);
-  const [filteredPois, setFilteredPois] = useState([]);
-  const [currentPoi, setCurrentPoi] = useState(null);
+const PoiListElement = ({ poi, onClick }) => {
+  return (
+    <div key={`poi-${poi.id}`} className="poi-selector__poi" onClick={onClick}>
+      <div className="poi-selector__poi__icon">
+        <img
+          className="poi-selector__poi__icon__image"
+          src={poi?.category.iconUrl}
+        />
+      </div>
+      <div className="poi-selector__poi__name">{poi.name}</div>
+      <div className="poi-selector__poi__type">{poi.category.title}</div>
+    </div>
+  );
+};
 
-  useEffect(() => {
-    setPois(buildings[0]?.pois.pois);
-    setCurrentPoi(pois.find((poi) => poi.id == currentPoiID));
-  }, [buildings, currentPoiID]);
-
-  function filterPois() {
-    // PRUEBA
-    const filtered = pois?.filter((poi) => {
+const PoiList = ({ pois, filterText, onClick }) => {
+  return pois
+    ?.filter((poi) => {
       return (
         filterText.trim() === "" ||
         poi.name.toLowerCase().includes(filterText.toLowerCase())
       );
-    });
+    })
+    .map((poi) => (
+      <PoiListElement key={poi.id} poi={poi} onClick={() => onClick(poi)} />
+    ));
+};
 
-    //setFilteredPois(filtered);
-    if (filteredPois.length > 0) {
-      return filtered.map((poi) => (
-        <div
-          key={`poi-${poi.id}`}
-          className="poi-selector__poi"
-          onClick={() => {
-            onSelect(poi);
-            setCurrentPoi(pois.find((p) => p.id == poi.id));
-          }}
-        >
-          <img
-            className="poi-selector__poi__icon"
-            src={poi?.category.iconUrl}
-          />
-          <div className="poi-selector__poi__name">{poi.name}</div>
-        </div>
-      ));
-    } else {
-      return (
-        <div className="poi-selector__no-pois">
-          No pois found matching the filter
-        </div>
-      );
-    }
-  }
-
-  return currentPoiID != null && currentPoi != null ? (
+const PoiDetails = ({ poi, buildings, onSelect }) => {
+  return (
     <div className="poi-selector">
       <div className="poi-selector__title">
         <div className="poi-selector__header">
@@ -64,15 +41,6 @@ const PoiSelector = ({
             src="https://situm.com/wp-content/themes/situm/img/logo-situm.svg"
             alt="Situm"
           />
-          <div className="poi-selector__title__poi">
-            <img
-              className="poi-selector__title__poi__icon"
-              src={currentPoi?.category.iconUrl}
-            />
-            <div className="poi-selector__title__poi__name">
-              {currentPoi?.name}
-            </div>
-          </div>
         </div>
 
         <BackIcon
@@ -81,8 +49,14 @@ const PoiSelector = ({
         />
       </div>
       <div className="poi-selector__info">
-        <div className="poi-selector__info__description">
-          {"que tienda tan bonita"}
+        <div className="poi-selector__poi">
+          <div className="poi-selector__poi__icon">
+            <img
+              className="poi-selector__poi__icon__image"
+              src={poi?.category.iconUrl}
+            />
+          </div>
+          <div className="poi-selector__poi__name">{poi?.name}</div>
         </div>
         <div className="poi-selector__info__position">
           <PositionIcon className="poi-selector__info__position position-icon" />
@@ -90,13 +64,39 @@ const PoiSelector = ({
             {buildings[0].name} - Level {""}
             {
               buildings[0].floors?.floors?.find(
-                (f) => f.id == currentPoi?.position.floor_id
+                (f) => f.id == poi?.position.floor_id
               ).level
             }
           </div>
         </div>
+        <div
+          className="poi-selector__info__description"
+          dangerouslySetInnerHTML={{
+            __html: poi?.info || "No additional information about this POI",
+          }}
+        ></div>
       </div>
     </div>
+  );
+};
+
+const PoiSelector = ({
+  buildings,
+  currentBuilding,
+  onSelect,
+  currentPoiID,
+}) => {
+  const [currentPoi, setCurrentPoi] = useState(null);
+  const [filterText, setFilterText] = useState("");
+  const [pois, setPois] = useState([]);
+
+  useEffect(() => {
+    setPois(buildings[0]?.pois.pois);
+    setCurrentPoi(pois.find((poi) => poi.id == currentPoiID));
+  }, [buildings, currentPoiID]);
+
+  return currentPoiID != null && currentPoi != null ? (
+    <PoiDetails poi={currentPoi} buildings={buildings} onSelect={onSelect} />
   ) : (
     <div className="poi-selector">
       <div className="poi-selector__header">
@@ -112,7 +112,6 @@ const PoiSelector = ({
             className="poi-selector__header__search__input"
             value={filterText}
             type="search"
-            data-action="search"
             placeholder="Search points of interest..."
             onChange={async (e) => {
               setFilterText(e.target.value);
@@ -122,40 +121,17 @@ const PoiSelector = ({
       </div>
       <div className="poi-selector__content">
         {pois?.length > 0 ? (
-          pois
-            ?.filter((poi) => {
-              return (
-                filterText.trim() === "" ||
-                poi.name.toLowerCase().includes(filterText.toLowerCase())
-              );
-            })
-            .map((poi) => (
-              <div
-                key={`poi-${poi.id}`}
-                className="poi-selector__poi"
-                onClick={() => {
-                  onSelect(poi);
-                  setCurrentPoi(pois.find((p) => p.id == poi.id));
-                }}
-              >
-                <img
-                  className="poi-selector__poi__icon"
-                  src={poi?.category.iconUrl}
-                />
-                <div className="poi-selector__poi__name">{poi.name}</div>
-              </div>
-            ))
+          <PoiList
+            pois={pois}
+            filterText={filterText}
+            onClick={(poi) => {
+              onSelect(poi);
+              setCurrentPoi(pois.find((p) => p.id == poi.id));
+            }}
+          />
         ) : (
           <div className="poi-selector__no-pois">No pois found</div>
         )}
-        {
-          // PRUEBA
-          /*pois?.length > 0 ? (
-          filterPois()
-        ) : (
-          <div className="poi-selector__no-pois">No pois found</div>
-        )*/
-        }
       </div>
     </div>
   );
